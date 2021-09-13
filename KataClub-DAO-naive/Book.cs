5bc18f8791e1b38513cada8705482a9c61f7e0af
Jennifer Anderson <andersonn.jen+github@gmail.com>
2021-09-12 22:13:37 -0500

    naive implementation

---
+++ KataClub-DAO-naive/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KataClub_DAO_naive
+{
+    public class Book
+    {
+
+        public int Isbn { get; set; }
+        public String BookName { get; set; }
+
+        public Book(int isbn, String bookName)
+        {
+            this.Isbn = isbn;
+            this.BookName = bookName;
+        }
+
+        //list is working as a database
+        private readonly List<Book> Books = new List<Book>
+            {
+                new Book(1, "Alice In Wonderland"),
+                new Book(2, "Cinderella"),
+                new Book(3, "The Three Little Bears")
+            };
+        
+
+        public List<Book> GetAllBooks()
+        {
+            return Books;
+        }
+
+        public Book GetBookByIsbn(int isbn)
+        {
+            return Books.Where(x => x.Isbn == isbn).FirstOrDefault();
+        }
+
+        public void SaveBook(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public void DeleteBook(Book book)
+        {
+            Books.Remove(book);
+        }
+    }
+}
---
+++ KataClub-DAO-naive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KataClub_DAO_naive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
